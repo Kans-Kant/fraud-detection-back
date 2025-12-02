69ae15f74e0b9bf91950d8313a0bd605b38d0375
Kans_Kant <kanssougiba627@gmail.com>
2025-12-02 02:52:22 +0100

    fix: config prod

--- src/test/java/com/isfa/fr/fraud_detection_back/FraudDetectionBackApplicationTests.java
+++ src/test/java/com/isfa/fr/fraud_detection_back/FraudDetectionBackApplicationTests.java
@@ -1,9 +1,10 @@
 package com.isfa.fr.fraud_detection_back;
 
+import com.isfa.fr.fraudDetection.FraudDetectionBackApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FraudDetectionBackApplication.class)
 class FraudDetectionBackApplicationTests {
 
 	@Test
