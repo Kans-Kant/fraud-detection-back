4cfd7a170fce85137ffea6c94c9894990ae8a097
Kans_Kant <kanssougiba627@gmail.com>
2025-12-02 04:53:13 +0100

    fix: config prod

--- src/main/java/com/isfa/fr/fraudDetection/controller/LogController.java
+++ src/main/java/com/isfa/fr/fraudDetection/controller/LogController.java
@@ -4,9 +4,11 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/log")
+@RestController
+@RequestMapping("/log")
 public class LogController {
 
     private static final Logger LOG = LogManager.getLogger(LogController.class);
